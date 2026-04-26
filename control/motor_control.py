2d889b4d84ea5b604230e606a9b6de4ced5d7b53
HuTao <3060954833@qq.com>
2026-04-26 15:29:38 +0800

    Update motor_control.py

---
+++ control/motor_control.py
@@ -0,0 +1,12 @@
+# motor_control.py
+# 小车控制模块入口
+
+def forward():
+    print("Car moves forward.")
+
+def stop():
+    print("Car stops.")
+
+if __name__ == "__main__":   如果 __name__ 等于“__main__”：
+    forward()
+    stop()
